66c9ee06a3f986adb3ac9260b8cb0378f54dd220
Rafael Grimmler da Rocha <grimmlerr@gmail.com>
2021-05-29 13:07:51 -0300

    versao 1.3

--- guerraNaval/src/guerranaval/GuerraNaval.java
+++ guerraNaval/src/guerranaval/GuerraNaval.java
@@ -8,13 +8,17 @@ package guerranaval;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 
-public class GuerraNaval extends JFrame implements ActionListener{
+public final class GuerraNaval extends JFrame implements ActionListener{
     //BOTÕES PRINCIPAIS
-    JButton sair, dica, disparoUnico, cascataHorizontal, cascataVertical, tiroEstrela;
-    JPanel painelBotoesCima, painelMapas, painelMapaEsquerda, painelMapaDireita;
-    Container tela;
+    public JButton sair, dica, disparoUnico, cascataHorizontal, cascataVertical, tiroEstrela;
+    public JPanel painelBotoesCima, painelMapas, painelEsquerda, painelDireita, painelMapaEsquerda, painelMapaDireita;
+    public Container tela;
+    //BOTÕES MAPA
+    public JButton[][] botaoMapaEsquerda = new JButton[10][10]; 
+    public JButton[][] botaoMapaDireita = new JButton[10][10]; 
+    //VARIAVEIS
+    public String tipoDisparo = "disparounico";
     
     public GuerraNaval(){
         super("Guerra Naval");
@@ -30,17 +34,166 @@ public class GuerraNaval extends JFrame implements ActionListener{
         painelBotoesCima.setLayout(new FlowLayout());
         
         painelMapas = new JPanel();        
-        painelMapaEsquerda = new JPanel();
-        painelMapaDireita = new JPanel();
+        painelEsquerda = new JPanel();
+        painelDireita = new JPanel();
         
         painelMapas.setLayout(new GridLayout(0, 2));
-        painelMapaEsquerda.setBackground(Color.red);
-        painelMapaDireita.setBackground(Color.BLUE);
+        painelEsquerda.setLayout(new BorderLayout());
+        painelDireita.setLayout(new BorderLayout());
+                
+        criarMapaEsquerda();
+        criarMapaDireita();
         
-        painelMapas.add(painelMapaEsquerda);
-        painelMapas.add(painelMapaDireita);
-              
-        // BOTÕES DO PAINEL DE CIMA
+        painelMapas.add(painelEsquerda);
+        painelMapas.add(painelDireita);
+       
+        criaBotoesCima();
+       
+        tela.add(painelBotoesCima, BorderLayout.NORTH);
+        tela.add(painelMapas);
+        
+        setSize(800, 600);
+        setVisible(true);
+        setLocationRelativeTo(null);  
+    }
+    
+    public void criarMapaEsquerda(){
+        /// -- ESTAO FUNÇÃO SERVE PARA DEFINIR O ESPAÇO DO BORDERLAYOUT.CENTER
+        /// ONDE ESTARÁ LOCALIZADO O MAPAS DA ESQUERDA.
+        JPanel auxiliar, painelbaixo, auxiliar1, paineldireita, auxiliar2, painelesquerda, auxiliar3, painelcima;
+        auxiliar = new JPanel();
+        JLabel player = new JLabel("                                                        Jogador");
+        painelbaixo = new JPanel();
+        painelbaixo.setLayout(new GridLayout(8, 0));
+        painelbaixo.add(auxiliar); 
+        painelbaixo.add(player);        
+        painelEsquerda.add(painelbaixo, BorderLayout.SOUTH);
+        ////
+        paineldireita = new JPanel();
+        auxiliar1 = new JPanel();
+        paineldireita.setLayout(new GridLayout(0, 5));
+        paineldireita.add(auxiliar1);        
+        painelEsquerda.add(paineldireita, BorderLayout.EAST);
+        ////
+        painelesquerda = new JPanel();
+        auxiliar2 = new JPanel();
+        painelesquerda.setLayout(new GridLayout(0, 5));
+        painelesquerda.add(auxiliar2);        
+        painelEsquerda.add(painelesquerda, BorderLayout.WEST);
+        ////
+        painelcima = new JPanel();
+        auxiliar3 = new JPanel();
+        painelcima.setLayout(new GridLayout(6, 0));
+        painelcima.add(auxiliar3);        
+        painelEsquerda.add(painelcima, BorderLayout.NORTH);
+        //// ADICIONA O PAINEL AONDE ESTARA OS BOTÕES DO MAPA
+        painelMapaEsquerda = new JPanel();
+        painelMapaEsquerda.setLayout(new GridLayout(11,11));
+        criaBotoesMapaEsquerdaEDireita("esquerda");
+        painelEsquerda.add(painelMapaEsquerda, BorderLayout.CENTER);
+    }
+       
+    public void criarMapaDireita(){
+        /// -- ESTAO FUNÇÃO SERVE PARA DEFINIR O ESPAÇO DO BORDERLAYOUT.CENTER
+        /// ONDE ESTARÁ LOCALIZADO O MAPAS DA DIREITA.
+        JPanel auxiliar, painelbaixo, auxiliar1, paineldireita, auxiliar2, painelesquerda, auxiliar3, painelcima;
+        auxiliar = new JPanel();
+        JLabel player = new JLabel("                                                        Computador");
+        painelbaixo = new JPanel();
+        painelbaixo.setLayout(new GridLayout(8, 0));
+        painelbaixo.add(auxiliar); 
+        painelbaixo.add(player);           
+        painelDireita.add(painelbaixo, BorderLayout.SOUTH);
+        ////
+        paineldireita = new JPanel();
+        auxiliar1 = new JPanel();
+        paineldireita.setLayout(new GridLayout(0, 5));
+        paineldireita.add(auxiliar1);        
+        painelDireita.add(paineldireita, BorderLayout.EAST);
+        ////
+        painelesquerda = new JPanel();
+        auxiliar2 = new JPanel();
+        painelesquerda.setLayout(new GridLayout(0, 5));
+        painelesquerda.add(auxiliar2);        
+        painelDireita.add(painelesquerda, BorderLayout.WEST);
+        ////
+        painelcima = new JPanel();
+        auxiliar3 = new JPanel();
+        painelcima.setLayout(new GridLayout(6, 0));
+        painelcima.add(auxiliar3);        
+        painelDireita.add(painelcima, BorderLayout.NORTH);
+        /// PAINEL ONDE SERA ADICIONADO OS BOTÕES DO MAPA
+        painelMapaDireita = new JPanel();
+        painelMapaDireita.setLayout(new GridLayout(11,11));
+        criaBotoesMapaEsquerdaEDireita("direita");
+        painelDireita.add(painelMapaDireita, BorderLayout.CENTER);
+    }
+    
+    public void criaBotoesMapaEsquerdaEDireita(String opcao){
+        Color personalizadaEsq = new Color(174,238,238);
+        Color personalizadaDir = new Color(225,255,175);
+        JLabel [] numeros = new JLabel[11];
+        JLabel [] letras = new JLabel[10];
+        String letra;
+        char c = 'A';
+        if( "esquerda".equals(opcao) ){
+            for( int i = 0; i < 11; i++ ){
+                if( i == 0 )
+                    numeros[i] = new JLabel("  ");
+                else
+                    numeros[i] = new JLabel("  " + i);
+                painelMapaEsquerda.add(numeros[i]);
+            }
+            ImageIcon img1 = new ImageIcon("icones/navioescolta1abatido.png");
+            ImageIcon img2 = new ImageIcon("icones/navioescolta2abatido.png");
+            ImageIcon img3 = new ImageIcon("icones/navioescolta3abatido.png");
+            for( int i = 0; i < 10; i++ ){
+                letra = "" + c++;
+                letras[i] = new JLabel(letra);
+                painelMapaEsquerda.add(letras[i]);
+                for( int j = 0; j < 10; j++ ){
+                    botaoMapaEsquerda[i][j] = new JButton();
+                    botaoMapaEsquerda[i][j].setBackground(personalizadaEsq);
+                    if( j >= 0 && j <= 2 ){
+                        if( j == 0 )
+                            botaoMapaEsquerda[i][j].setIcon(img1);
+                        if( j == 1 )
+                            botaoMapaEsquerda[i][j].setIcon(img2);
+                        if( j == 2 )
+                            botaoMapaEsquerda[i][j].setIcon(img3);
+                    }
+                    painelMapaEsquerda.add(botaoMapaEsquerda[i][j]);
+                }
+            }
+        }
+        else{
+            String comando;
+            int contadorComando = 0;
+            for( int i = 0; i < 11; i++ ){
+                if( i == 0 )
+                    numeros[i] = new JLabel("  ");
+                else
+                    numeros[i] = new JLabel("  " + i);
+                painelMapaDireita.add(numeros[i]);
+            }
+            for( int i = 0; i < 10; i++ ){
+                letra = "" + c++;
+                letras[i] = new JLabel(letra);
+                painelMapaDireita.add(letras[i]);
+                for( int j = 0; j < 10; j++ ){
+                    botaoMapaDireita[i][j] = new JButton();
+                    botaoMapaDireita[i][j].setBackground(personalizadaDir);
+                    comando = "" + contadorComando++;
+                    botaoMapaDireita[i][j].setActionCommand(comando);
+                    botaoMapaDireita[i][j].addActionListener(this);
+                    painelMapaDireita.add(botaoMapaDireita[i][j]);
+                }
+            }
+        }
+    }
+    
+    public void criaBotoesCima(){
+           // BOTÕES DO PAINEL DE CIMA
         sair = new JButton("Sair");
         sair.setBackground(Color.WHITE);
         dica = new JButton("Dica");
@@ -75,54 +228,112 @@ public class GuerraNaval extends JFrame implements ActionListener{
         painelBotoesCima.add(cascataHorizontal);
         painelBotoesCima.add(cascataVertical);
         painelBotoesCima.add(tiroEstrela);
-        tela.add(painelBotoesCima, BorderLayout.NORTH);
-        tela.add(painelMapas);
-        
-        setSize(800, 600);
-        setVisible(true);
-        setLocationRelativeTo(null);  
     }
     
-    public static void main(String[] args) {
-        menuPrincipal guiMenu = new menuPrincipal();
-        guiMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         String comando = e.getActionCommand();
         if( comando.contains("P.") ){
-            // TODA VEZ QUE ACONTECER UMA AÇÃO NOS BOTOES REDEFINE A COR
-            dica.setBackground(Color.WHITE);
-            dica.setForeground(Color.BLACK);
-            disparoUnico.setBackground(Color.WHITE);
-            disparoUnico.setForeground(Color.BLACK);
-            cascataHorizontal.setBackground(Color.WHITE);
-            cascataHorizontal.setForeground(Color.BLACK);
-            cascataVertical.setBackground(Color.WHITE);
-            cascataVertical.setForeground(Color.BLACK);
-            tiroEstrela.setBackground(Color.WHITE);
-            tiroEstrela.setForeground(Color.BLACK);
+            transformaPadraoBotoes();
             //-------------------------------
-            if( "P.sair".equals(comando) ){
-                System.exit(0);          
-            }else if( "P.dica".equals(comando) ){
-                dica.setBackground(Color.DARK_GRAY);
-                dica.setForeground(Color.WHITE);
-            }else if( "P.disparounico".equals(comando) ){
-                disparoUnico.setBackground(Color.DARK_GRAY);
-                disparoUnico.setForeground(Color.WHITE);
-            }else if( "P.cascatahorizontal".equals(comando) ){
-                cascataHorizontal.setBackground(Color.DARK_GRAY);
-                cascataHorizontal.setForeground(Color.WHITE);
-            }else if( "P.cascatavertical".equals(comando) ){
-                cascataVertical.setBackground(Color.DARK_GRAY);
-                cascataVertical.setForeground(Color.WHITE);
-            }else if( "P.tiroestrela".equals(comando) ){
-                tiroEstrela.setBackground(Color.DARK_GRAY);
-                tiroEstrela.setForeground(Color.WHITE);
+            switch (comando) {
+                case "P.sair":
+                    System.exit(0);
+                case "P.dica":
+                    dica.setBackground(Color.DARK_GRAY);
+                    dica.setForeground(Color.WHITE);
+                    break;
+                case "P.disparounico":
+                    disparoUnico.setBackground(Color.DARK_GRAY);
+                    disparoUnico.setForeground(Color.WHITE);
+                    tipoDisparo = "disparounico";
+                    break;
+                case "P.cascatahorizontal":
+                    cascataHorizontal.setBackground(Color.DARK_GRAY);
+                    cascataHorizontal.setForeground(Color.WHITE);
+                    tipoDisparo = "cascatahorizontal";
+                    break;
+                case "P.cascatavertical":
+                    cascataVertical.setBackground(Color.DARK_GRAY);
+                    cascataVertical.setForeground(Color.WHITE);
+                    tipoDisparo = "cascatavertical";
+                    break;
+                case "P.tiroestrela":
+                    tiroEstrela.setBackground(Color.DARK_GRAY);
+                    tiroEstrela.setForeground(Color.WHITE);
+                    tipoDisparo = "tiroestrela";
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if( comando.matches("-?\\d+") ){
+            int posicao, contadorx = 0, contadory = 0, limite = 9;
+            posicao = Integer.parseInt(comando);
+            for(int i = 0; i < posicao; i++ ){
+                contadory = contadory + 1;
+                if( i == limite ){
+                    limite = limite + 10;
+                    contadory = 0;
+                    contadorx = contadorx + 1;
+                }
+            }
+            if( "disparounico".equals(tipoDisparo) ){
+                botaoMapaDireita[contadorx][contadory].setActionCommand("");
+                botaoMapaDireita[contadorx][contadory].setBackground(Color.DARK_GRAY);
+            }else if( "cascatahorizontal".equals(tipoDisparo) ){
+                botaoMapaDireita[contadorx][contadory].setActionCommand("");
+                botaoMapaDireita[contadorx][contadory].setBackground(Color.DARK_GRAY);
+                if( contadory + 1 < 10 ){
+                    botaoMapaDireita[contadorx][contadory+1].setActionCommand("");
+                    botaoMapaDireita[contadorx][contadory+1].setBackground(Color.DARK_GRAY);
+                }
+            }else if( "cascatavertical".equals(tipoDisparo) ){
+                botaoMapaDireita[contadorx][contadory].setActionCommand("");
+                botaoMapaDireita[contadorx][contadory].setBackground(Color.DARK_GRAY);
+                if( contadorx - 1 > -1 ){
+                    botaoMapaDireita[contadorx-1][contadory].setActionCommand("");
+                    botaoMapaDireita[contadorx-1][contadory].setBackground(Color.DARK_GRAY);
+                }
+            }else if( "tiroestrela".equals(tipoDisparo) ){
+                botaoMapaDireita[contadorx][contadory].setActionCommand("");
+                botaoMapaDireita[contadorx][contadory].setBackground(Color.DARK_GRAY);
+                if( contadorx - 1 > -1 ){
+                    botaoMapaDireita[contadorx-1][contadory].setActionCommand("");
+                    botaoMapaDireita[contadorx-1][contadory].setBackground(Color.DARK_GRAY);
+                }
+                if( contadorx + 1 < 10 ){
+                    botaoMapaDireita[contadorx+1][contadory].setActionCommand("");
+                    botaoMapaDireita[contadorx+1][contadory].setBackground(Color.DARK_GRAY);
+                }
+                if( contadory + 1 < 10 ){
+                    botaoMapaDireita[contadorx][contadory+1].setActionCommand("");
+                    botaoMapaDireita[contadorx][contadory+1].setBackground(Color.DARK_GRAY);
+                }
+                if( contadory - 1 > -1 ){
+                    botaoMapaDireita[contadorx][contadory-1].setActionCommand("");
+                    botaoMapaDireita[contadorx][contadory-1].setBackground(Color.DARK_GRAY);
+                }
             }
         }
     }
     
+    public void transformaPadraoBotoes(){
+        // TODA VEZ QUE ACONTECER UMA AÇÃO NOS BOTOES REDEFINE A COR
+        dica.setBackground(Color.WHITE);
+        dica.setForeground(Color.BLACK);
+        disparoUnico.setBackground(Color.WHITE);
+        disparoUnico.setForeground(Color.BLACK);
+        cascataHorizontal.setBackground(Color.WHITE);
+        cascataHorizontal.setForeground(Color.BLACK);
+        cascataVertical.setBackground(Color.WHITE);
+        cascataVertical.setForeground(Color.BLACK);
+        tiroEstrela.setBackground(Color.WHITE);
+        tiroEstrela.setForeground(Color.BLACK);
+    }
+    
+     public static void main(String[] args) {
+        menuPrincipal guiMenu = new menuPrincipal();
+        guiMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    }
 }
